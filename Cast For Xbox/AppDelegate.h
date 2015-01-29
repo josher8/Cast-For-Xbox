3017718ca4524d1fbbcbcda2f5fcb284a5351521
Josher Slebodnik <josh.slebodnik@forumcomm.com>
2015-01-29 11:39:26 -0600

    Initial Commit

---
+++ Cast For Xbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cast For Xbox
+//
+//  Created by Josher Slebodnik on 1/29/15.
+//  Copyright (c) 2015 Josher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cast For Xbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cast For Xbox
+//
+//  Created by Josher Slebodnik on 1/29/15.
+//  Copyright (c) 2015 Josher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
